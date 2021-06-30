95c739f25ebf98f86478e87b53ee867f93c15f38
Justin <ro0566320@gmail.com>
2021-06-29 23:55:55 -0400

    better solution

--- Convert string to camel case/better solution.js
+++ Convert string to camel case/better solution.js
@@ -1,6 +1,7 @@
 function toCamelCase(str){
     var regExp=/[-_]\w/ig;
     return str.replace(regExp,function(match){
+         console.log('test')
           return match.charAt(1).toUpperCase();
      });
 }
